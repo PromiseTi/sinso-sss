6e84763645333143b148571720d3b0f9f830efe8
787814925 <862856117@qq.com>
2021-10-15 23:39:56 +0800

    svg 修改

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '星硕',
+  title: 'SINSO',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import styles from './index.module.css'
 import HomepageFeatures from '../components/HomepageFeatures'
 
+import Img1 from '../../static/img/content/ban2.svg'
+import Img2 from '../../static/img/content/ban1.svg'
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
@@ -17,9 +19,11 @@ function HomepageHeader() {
             <h1> HeathCare + </h1>
             <h1>NFT Infrastructure</h1>
             <p>Let human beings enter the era of democratic medical care</p>
-            <img src="/static/img/content/img-banner-t2.png" />
+            <Img1 />
           </div>
-          <img src="/static/img/content/img-banner-t1.png" />
+          <div className="img">
+            <Img2 />
+          </div>
         </div>
         {/* <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -40,7 +44,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`SINSO`}
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
